6b87e8c82919ab369246faba644a634986a47f80
andela-obogbare <alexonozor@gmail.com>
2015-07-15 18:33:43 +0100

    added images

--- config/application.rb
+++ config/application.rb
@@ -19,9 +19,8 @@ module EventX
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.serve_static_assets = true
-    config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
     config.assets.compile = true
+    config.assets.digest = true
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
