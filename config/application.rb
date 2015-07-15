0f59f22afa95ca894bde8b62f3504508b4ca7c95
andela-obogbare <alexonozor@gmail.com>
2015-07-15 18:23:51 +0100

    added compiler to application.rb

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,9 @@ module EventX
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+    config.serve_static_assets = true
+    config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+    config.assets.compile = true
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
