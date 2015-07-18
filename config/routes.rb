bf7a9092ebffdffe8a651f9b1ef52bf799fdcf0c
Oreoluwa Akinniranye <oreoluwa.akinniranye@andela.com>
2015-07-18 11:59:36 +0100

    changed the route naming, aliasing the create_events path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  get 'events/create_event'
+  get '/create_event' => 'events#create_event', as: "create_event"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
