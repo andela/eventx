f868c33a60e7b020608e756ae96574014f9242e6
Oreoluwa Akinniranye <oreoluwa.akinniranye@andela.com>
2015-07-23 16:34:53 +0100

    worked on the navbar
    	--set a static size for the nav user image
    	--removed the console output in the user model, which was used
    for testing
    	--removed the login icon on mobile, but login now shows on
    mobile if user is not logged in
    	--removed extra comment line in the omniauth.rb file
    	--more changes to come

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,14 +12,12 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      puts "auth request ", auth.inspect
       user.provider = auth.provider
       user.uid = auth.uid
       user.profile_url = auth.info.image
       user.first_name = auth.info.name
       user.email = auth.info.email
       user.oauth_token = auth.credentials.token
-      #user.oauth_token_expires_at = auth.credentials.expires.nil? ? nil : Time.at(auth.credentials.expires_at)
       user.save!
     end
   end
