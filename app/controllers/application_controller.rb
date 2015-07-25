a3187702f5a3ce5e2a71b8cc6dc5cf3c39bbfb1e
Oreoluwa Akinniranye <oreoluwa.akinniranye@andela.com>
2015-07-25 22:20:42 +0100

    Defined a new helper method for the login_providers
    Initially, the login provider had to be written in a long html way,
    however this didn't seem DRY, therefore I created a new helper method
    which would would loop through all the providers, which has been
    predefined in the ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   @categories = {tunde: 1, ore: 2, kay: 3, gibbs: 4}
   @val = 1
-  helper_method :current_user
+  helper_method :current_user, :sign_in_provider
   protect_from_forgery with: :exception
 
 
@@ -15,5 +15,16 @@ class ApplicationController < ActionController::Base
 
   def redirect_to_user_sign_in
     redirect_to root_path if !current_user
+  end
+
+  def sign_in_provider
+    {
+      facebook: {name: "Facebook", class: "blue darken-3", icon_class: "fa-facebook"},
+      twitter: {name: "Twitter", class: "blue", icon_class: "fa-twitter"},
+      google_oauth2: {name: "Google", class: "red darken-4", icon_class: "fa-google-plus"},
+      linkedin: {name: "LinkedIn", class: "blue darken-4", icon_class: "fa-linkedin"},
+      github: {name: "Github", class: "grey darken-4", icon_class: "fa-github"},
+      tumblr: {name: "Tumblr", class: "blue-grey darken-3", icon_class: "fa-tumblr"}
+    }
   end
 end
