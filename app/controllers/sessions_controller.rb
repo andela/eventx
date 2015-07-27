91ff015b6e555547f2ac66d6e1509af6aa30898e
Oreoluwa Akinniranye <oreoluwa.akinniranye@andela.com>
2015-07-27 14:39:51 +0100

    indented blocks in application.js, added an origin query_string to the signin_path helper and sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,16 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env['omniauth.auth'])
     session[:user_id] = user.id
-    if session[:url].nil?
+    if env['omniauth.origin'].nil?
       redirect_to root_url
     else
-      redirect_to session[:url]
+      redirect_to env['omniauth.origin']
     end
+    # if session[:url].nil?
+    #   redirect_to root_url
+    # else
+    #   redirect_to session[:url]
+    # end
   end
 
   def destroy
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,14 @@
 module ApplicationHelper
   def signin_path(provider)
-    "/auth/#{provider.to_s}"
+    redir_path = {origin: request.env['PATH_INFO']}.to_query
+    # puts "oreoluwa #{redir_path}"
+    "/auth/#{provider.to_s}?#{redir_path}"
   end
 
   def create_event_or_login(name)
     classes = ['waves-effect', 'waves-light btn-large',  'home_button']*' '
     if current_user
-      content_tag(:a, name, href:events_new_path, class: classes )
+      content_tag(:a, name, href: events_new_path, class: classes)
     else
       content_tag(:a, name, href:'#login_modal', class: classes + ' modal-trigger' )
     end
