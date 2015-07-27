2c79d28cf6e8dd4a154e48c519730c8b7a07634e
Oreoluwa Akinniranye <oreoluwa.akinniranye@andela.com>
2015-07-27 09:27:42 +0100

    Worked a bit more on the scroll notifier
    The scroll notifier was initially static, however this commit adds a bit
    of animation to the notifier amongst other things
    
    Also worked a bit on the create events page, however these may change if
    alex has a better interface as he has also worked on the create events
    page a bit.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
-  before_action :redirect_to_user_sign_in, only: [:new]
-  
+  before_action :redirect_to_user_sign_in, only: [:new] if Rails.env.production?
+
   def new
   end
 
