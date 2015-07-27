28dc14198d29443295f3c2a7f8f3fd025a830c68
Kayode Adeniyi <kayode.adeniyi@andela.co>
2015-07-27 08:43:59 +0100

    Changed the color of the search button and tab on the index page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -56,10 +56,10 @@ $(document).ready(function() {
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-            /* If the object is completely visible in the window, fade it it */
+            /* If the object is completely visible in the window, fade it in */
             if( bottom_of_window > bottom_of_object ){
 
-                $(this).animate({'opacity':'1'},500);
+                $(this).animate({'opacity':'1'},300);
 
             }
 
