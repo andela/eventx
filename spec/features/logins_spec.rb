877debab2550fb6584e14a1881141542b791497b
Kayode Adeniyi <kayode.adeniyi@andela.co>
2015-07-28 11:26:50 +0100

    Updated the test

--- spec/features/logins_spec.rb
+++ spec/features/logins_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "Logins", type: :feature do
     visit root_path
 
     expect(page).to have_selector("h1", text: "EventX")
-    expect(page).to have_selector("", text: "EventX")
+    #expect(page).to have_selector("", text: "EventX")
   end
   #pending "add some scenarios (or delete) #{__FILE__}"
 end
