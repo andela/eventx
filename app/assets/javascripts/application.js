ffd022ae929227413b880a017c380a3a274f3369
Kayode Adeniyi <kayode.adeniyi@andela.co>
2015-08-11 21:30:06 +0100

    Fixed the overlap issue with footer and sidebar on the admin page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -200,3 +200,28 @@ $(document).ready(function(){
 })
 
 
+$(document).ready(function () {
+    $(window).scroll(function () {
+      var height = $('#content').height();
+      var scroll = $(this).scrollTop();
+      var win = $(window).height();
+      var nav = $('.nav-wrapper').height();
+      var height2 = height + 6 + nav - win
+
+      if(scroll - (height - win) >= 130){
+        $('#slide-out').css({
+          'position': 'absolute',
+          'bottom': '0',
+          'margin-top': height2,
+          'left': '0',
+          'z-index': '-1'
+        });
+      }else {
+        $('#slide-out').css({
+            'position': 'fixed',
+            'margin-top' : '0',
+            'z-index': '200'
+        });
+      }
+    });
+});
