4566c090a6189013149a67ae3b1032a35da598ab
Stephen <stephen.sunday@andela.co>
2015-08-12 10:41:04 +0100

    Add minitest for event create

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class EventsControllerTest < ActionController::TestCase
-
-
+ 
+  test 'should get new' do
+    User.create({id: 2, first_name: 'OreO' })
+    session[:user_id] = 2
+    get :new
+    assert_response :success
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Create a user' do
+    user = User.create({id: 2, first_name: 'OreO' })
+
+    assert_equal user.id, 2
+  end
 end
