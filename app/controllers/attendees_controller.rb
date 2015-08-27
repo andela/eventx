324d377006624827c4087dd309b297cd30681b7e
Kayode Adeniyi <kayode.adeniyi@andela.co>
2015-08-27 13:49:39 +0100

    Refactored Ajax call from loading a div to adding a 'hide' class to 'attend' button and removing a 'hide' class from 'unattend' button. This was done in Attendee coffescript

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -1,19 +1,22 @@
 class AttendeesController < ApplicationController
-  respond_to :js
+  #respond_to :js
   before_action :find_event
   def create
     @attendee = @event.attendees.build(user_id: current_user.id)
     if @attendee.save
-      @create2 = true
+      #@create2 = true
     else
-      @create2 = false
+      #@create2 = false
     end
   end
 
   def destroy
    @attendee = current_user.attendees.find_by_event_id(@event.id)
-   @destroy = true if @attendee.destroy
-    flash[:error] = "You have successfully unattend this event"
+   if !@attendee.nil? && @attendee.destroy
+     #flash[:notice] = "You have successfully unattend this event"
+   else
+     #flash[:notice] = "An error occurred"
+   end
   end
 
   private
