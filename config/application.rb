b1b18f6cc208dbe83cd78f2e1335234d5614d749
Kayode Adeniyi <kayode.adeniyi@andela.co>
2015-09-23 12:56:29 +0100

    Fix seed: reset primary key sequence after emptying a table before seed

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module EventX
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    # config.autoload_paths += Dir["#{config.root}/lib/**/"]
     config.assets.compile = true
     config.assets.digest = true
     # Do not swallow errors in after_commit/after_rollback callbacks.
