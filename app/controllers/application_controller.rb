c475d0ee5311b92bc6ed06fd0f26a343a9a54bf4
Oreoluwa Akinniranye <oreoluwa.akinniranye@andela.com>
2015-09-23 15:24:13 +0100

    fixed the user_id which was hardcoded to the event page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-  @color = "red"
   #private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
