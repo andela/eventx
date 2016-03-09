9fbf89f7043c8cad29630428e8c38e7cbf1d8692
Oduola Akeem <akeem.oduola@andela.com>
2016-03-09 16:22:31 +0100

    Move faker out of development

--- config/boot.rb
+++ config/boot.rb
@@ -2,7 +2,7 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../Gemfile", __FILE__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require 'rails/commands/server'
-<<<<<<< HEAD
+
 module Rails
   class Server
     def default_options
@@ -10,13 +10,3 @@ module Rails
     end
   end
 end
-=======
-
-module Rails
-  class Server
-    def default_options
-      super.merge(Host: '0.0.0.0', Port: 3000)
-    end
-  end
-end
->>>>>>> 1cdd5821c4a321755d86964c4543128267904a4d
