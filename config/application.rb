1505391129ebd942631cd99f7e5e7a81f2110653
Femi Senjobi <femi.senjobi@andela.com>
2016-04-14 12:38:19 +0100

    refactor(app): remove zombie code
    delete commented codes
    
    [#117445875[]]

--- config/application.rb
+++ config/application.rb
@@ -3,39 +3,15 @@ require File.expand_path("../boot", __FILE__)
 require "rails/all"
 require "wicked_pdf"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-
 Bundler.require(*Rails.groups)
 
 module EventX
   class Application < Rails::Application
-    # Use the responders controller from the responders gem
-    config.app_generators.scaffold_controller :responders_controller
 
-    # Settings in config/environments/*
-    # take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone
-    # and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names.
-    # Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en
-    # and all translations from config/locales/*.rb,yml
-    # are auto loaded.
-    # config.i18n.load_path +=
-    # Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-    # config.autoload_paths << Rails.root.join("lib")
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
     config.autoload_paths += Dir["#{config.root}/lib/*"]
     config.assets.compile = true
     config.assets.digest = true
-    # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.middleware.use WickedPdf::Middleware, {},
                           only: ["/print", "/download"]
