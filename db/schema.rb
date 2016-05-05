5c1626acd89069ce5557a3d55a851b3154499cfa
Femi Senjobi <femi.senjobi@andela.com>
2016-05-05 16:29:42 +0100

    feat(tickets): create migration for user_ticket
    add is_used column to know if ticket has been used
    add time_used column to know when ticket was used
    add scanned_by column to know who scanned the ticket
    
    [Starts #108727880]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505091750) do
+ActiveRecord::Schema.define(version: 20160505072732) do
 
   create_table "attendees", force: :cascade do |t|
     t.integer  "user_id"
