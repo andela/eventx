9906742bad419848325f696fd063fec1afc8d9b3
andela-oesho <susan.esho@andela.com>
2016-05-16 14:31:34 +0100

    feat (popular_events) display slides of popular events
    add new style and slide to the popular events page
    
    [delivers #103821188]

---
+++ app/assets/javascripts/slider.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+ $('.slider').slider();
+ $('.slider').slider('pause');
+ $('.slider').slider('start');
+ $('.slider').slider('next');
+ $('.slider').slider('prev');
+});
