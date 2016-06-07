4cf63df8faaf6edb9904e69e88e63972df30c69c
Femi Senjobi <femi.senjobi@andela.com>
2016-06-07 17:02:36 +0100

    feat(bookings): add javascript file for refund
    [#101390216]

---
+++ app/assets/javascripts/request_refund.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  $('.refund-button').click(function (){
+    var uniq_id = $(this).data('id');
+    $('#uniq_id').val(uniq_id);
+  });
+
+  $('#reason-label').hide();
+  $('input[type="radio"]').click(function (){
+    if ($('#reason-other').is(':checked')) {
+      $('#reason-label').show();
+    }else {
+      $('#reason-label').hide();
+    }
+  });
+
+  $('#submit-refund').click(function (){
+    var reason = "Event was Cancelled"
+    var uniq_id = $('#uniq_id').val();
+    if ($("#reason-custom").val() !== ""){
+      reason = $("#reason-custom").val();
+    }
+    $.ajax({
+      url: '/refund/' + uniq_id,
+      method: 'POST',
+      data: { reason: reason }
+    })
+    .done(function(){
+      Materialize.toast('Your request for refund has been submitted', 3000);
+      var ele = $('*[data-id="' + uniq_id + '"]');
+      ele.html('Processing Request');
+      ele.attr('class','btn disabled print-box-size');
+      ele.attr('href','#');
+    })
+    .fail(function(){
+      Materialize.toast('Sorry, request for refund was not submitted', 3000);
+    });
+  });
+});
