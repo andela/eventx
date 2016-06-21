e3b51f4b12371335d9bff6bfb18766b502cc827f
Ogbara Godwin <godwin.onisofien@andela.com>
2016-06-21 11:12:46 +0100

    refactor(reviews): handle style violations in review js file

--- app/assets/javascripts/review.js
+++ app/assets/javascripts/review.js
@@ -1,50 +1,57 @@
 $(document).ready(function() {
-  $('#addReviewForm').submit(function (){
+  $('#addReviewForm').submit(function (event){
       var body, event_id, user_id, rating, reviewers_name;
-      body = $("#reviewBody").val()
-      event_id = $('#reviewEventId').val();
-      user_id = $('#reviewerId').val();
+      body = $("#reviewBody").val();
+      event_id = $("#reviewEventId").val();
+      user_id = $("#reviewerId").val();
       rating = $( "input:checked" ).val();
+
       $.ajax({
-        url: '/events/' + event_id + '/reviews',
-        type: 'POST',
-        data: { review: { body: body, event_id: event_id, rating: rating, user_id: user_id } }
+        url: "/events/" + event_id + "/reviews",
+        type: "POST",
+        data: {
+          review: {
+            body: body,
+            event_id: event_id,
+            rating: rating,
+            user_id: user_id
+          }
+        }
       })
+
       .done(function(data){
-        if(data.error){
-          Materialize.toast(data.error, 3000)
-        }else{
-          Materialize.toast('Your review has been saved', 3000);
-          var clonedDiv = $('#review').clone();
+          Materialize.toast("Your review has been saved", 3000);
+          var clonedDiv = $("#review").last().clone();
           clonedDiv.find("#reviewBodyContent").html(data.body)
           filled_in_stars(data.rating, clonedDiv);
           empty_stars(data.rating, clonedDiv);
           add_author_image(clonedDiv);
           add_author_name(clonedDiv);
           clonedDiv.removeAttr("style");
-          $('#review').after(clonedDiv);
-        }
+          clonedDiv.insertAfter($("#review").last());
+          empty_fields();
       })
+
       .fail(function(){
-        Materialize.toast('Sorry, review cannot be saved', 3000);
+        Materialize.toast("Sorry, review cannot be saved", 3000);
       });
 
-      event.preventDefault()
+      event.preventDefault();
     });
 
     function filled_in_stars(number, target_div){
-      var stars = '';
+      var stars = "";
       for(var i = 1; i <= number; i++){
-        stars += '<span class="filled-in-star star-size">&#9734;</span>';
+        stars += "<span class='filled-in-star star-size'>&#9734;</span>";
       }
       target_div.find(".review-rating").html(stars);
     };
 
     function empty_stars(number, target_div){
-      var stars = '';
-      count = 5 - number
+      var stars = "";
+      count = 5 - number;
       for(var i = 1; i <= count; i++){
-        stars += '<span class="star-size">&#9734;</span>';
+        stars += "<span class='star-size'>&#9734;</span>";
       }
       target_div.find(".review-rating").append(stars);
     };
@@ -57,6 +64,11 @@ $(document).ready(function() {
 
     function add_author_name(target_div){
       reviewers_name = $("a.dropdown-button").text().trim();
-      target_div.find(".review-author-name").html(reviewers_name)
+      target_div.find(".review-author-name").html(reviewers_name);
+    };
+
+    function empty_fields(){
+      $("#reviewBody").val("");
+      $( "input:checked" ).attr("checked", false);
     }
 });
