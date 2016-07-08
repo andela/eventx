d9627c3a8d99f0f2a138ae3cfc823d7073e94602
andela-sdamian <simon.peter@andela.com>
2016-07-08 11:32:47 +0100

    Delete all spec in test file
    Why?
    we don't use test unit with with eventx, this test files were
    auto-generated by rails scaffold.

--- test/mailers/previews/ticket_transaction_mailer_preview.rb
+++
@@ -1,6 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/ticket_transaction_mailer
-class TicketTransactionMailerPreview < ActionMailer::Preview
-  def transfer_mail 
-    TicketTransactionMailer.transfer_mail(options)
-  end 
-end
