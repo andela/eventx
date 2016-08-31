afbb7d419b839243a3ad3236d83afa2d282f097f
Chineze Nwosu <chineze.nwosu@andela.com>
2016-08-31 08:55:34 +0100

    [#126584711] change variable name from presenter to milestone_presenter

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -19,7 +19,7 @@ class WelcomeController < ApplicationController
   end
 
   def about
-    @presenter = Welcome::WelcomePresenter.new
+    @milestone_presenter = Welcome::WelcomePresenter.new
   end
 
   def renders(events)
